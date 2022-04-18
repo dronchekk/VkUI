42929b01c57332bb594bbd3767194e2fad8fb665
Andrey rachitsky <>
2022-04-18 12:52:03 +0300

    new version

---
+++ VKUI/VKUI/VKUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKUIApp.swift
+//  VKUI
+//
+//  Created by Andrey Rachitskiy on 18.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
